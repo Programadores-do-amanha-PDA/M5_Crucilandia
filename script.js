18d06126183a16e74b1328d5575bf38edb95672f
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-09-01 20:06:06 -0300

    feat: commit inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Criação de um array simples
+//Crie um array chamado frutas com pelo menos 5 frutas diferentes.
+//👉 Exiba todas elas no console usando um laço for.
+
+let frutas = ['banana', 'manga', 'laranja', 'melão', 'morango', 'pera', 'melancia'];
+
+for(let i = 0; i < frutas.length; i = i + 1){
+  console.log(frutas[i] + " " + "a variável i passou por aqui")
+}
